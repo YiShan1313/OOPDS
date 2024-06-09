03f620971611a7767957cfa145013241ad8a3715
kirito-sy <79395542+kirito-sy@users.noreply.github.com>
2024-06-09 18:37:30 +0800

    Create Robot.h

---
+++ Robot.h
@@ -0,0 +1,80 @@
+
+#ifndef Robot_H
+#define Robot_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Battlefield; 
+
+class Robot {
+private:
+    string name;
+    int live;
+    int score;
+    int pos_X;
+    int pos_Y;
+
+public:
+    Robot(string n, int x, int y) : name(n), live(3), pos_X(x), pos_Y(y), score(0) {}
+    virtual ~Robot() {}
+
+    int getPos_X() const {
+        return pos_X;
+    }
+    int getPos_Y() const {
+        return pos_Y;
+    }
+
+    string getName() const {
+        return name;
+    }
+
+    void setName(const string& n) {
+        name = n;
+    }
+
+    void setPosition(int x, int y) {
+        pos_X = x;
+        pos_Y = y;
+    }
+
+    int getScore()
+    {
+        return score;
+    }
+
+    void Score()
+    {
+        score ++;
+        cout << getName() << " score " << score << endl;
+        if (score == 3)
+        {
+        }
+    }
+
+    int getLive(){
+        return live;
+    }
+
+    void killed() {
+        live --;
+        cout << getName() << " your lives left " << live << endl;
+        if (live == 0)
+        {
+            setPosition(-1, -1);
+            cout << "dead" << endl;
+        }
+        
+    }
+
+
+    // virtual void operation(Battlefield &Test)
+    // {}
+
+
+    virtual void operation(Battlefield &Test) = 0;
+};
+
+#endif
